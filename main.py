5b0f6ae9cc97136fa3f0aa24bcea327358fb5e81
מיכאל <abbcnn220@gmail.com>
2023-04-23 00:15:43 +0300

    v 1.10

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import time
 
-a1 = np.array([200.9,700.6,600.4,340.7])
+a1 = np.array([1,0,1,0])
 
-print(a1.astype('int'))
+print(a1.astype('bool'))
 print((a1).shape)
