10e39b4d22c95cca9b9a06a4052c3fd95c3dd22c
מיכאל <abbcnn220@gmail.com>
2023-04-23 02:36:25 +0300

    v 1.13

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 import sys
 import time
-
-o =  np.linspace(0,100,7)
+o =np.full((2,3),7)
 print(o)
+
+o1 =np.full((2,5),np.random.randint(1,10))
+print(o1)
+
+o2 = np.random.randint(0,10,size=(3,4))
+print(o2)
+print(o2.T)
