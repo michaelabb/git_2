d1382eee6daf8815b169c52661a362003c7749f1
מיכאל <abbcnn220@gmail.com>
2023-04-23 00:12:16 +0300

    v 1.9

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import time
 
-a1 = np.array(range(27))
+a1 = np.array([200.9,700.6,600.4,340.7])
 
-print(a1.reshape((3,3,3)))
+print(a1.astype('int'))
 print((a1).shape)
