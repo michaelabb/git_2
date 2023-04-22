df399b48811b31a53c7e4e5b0a54b5146305ae4a
מיכאל <abbcnn220@gmail.com>
2023-04-23 02:14:18 +0300

    v 1.13

--- main.py
+++ main.py
@@ -2,9 +2,5 @@ import numpy as np
 import sys
 import time
 
-a1 = np.array([1,2,3,4,5,6])
-a2 = np.array([1,2,3,9,3,6])
-
-print(np.product(a1>a2))
-print(np.where(a1==a2))
-print(np.where(a1>a2))
+o =  np.linspace(0,100,7)
+print(o)
