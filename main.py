ec7a0c8621349882d74f17f1c9fa24cc7c2a1d96
מיכאל <abbcnn220@gmail.com>
2023-04-22 23:57:49 +0300

    v 1.7

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import numpy as np
 import sys
 import time
 
-tempratures = np.array([29.3,42.1,18.8,38.0,12.5,12.6,12.7,49.9,38.6,31.3,9.2,22.2]).reshape(2,3,2)
-print(tempratures)
-print('----------')
-tempratures = np.swapaxes(tempratures,1,2)
-print(tempratures)
+a1 = np.array([[1,2,3],[4,5,6]])
+a2 = np.array([[7,8,9],[10,11,12]])
+print((a1+a2)*3.14)
+print((a1+a2).shape)
