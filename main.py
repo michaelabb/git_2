d1389a745c432d75b557ca9704d230de6f45b8b5
מיכאל <abbcnn220@gmail.com>
2023-04-23 02:44:58 +0300

    v 1.14

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import numpy as np
 import sys
 import time
-o =np.full((2,3),7)
+o =np.random.randint(100,size=10)
 print(o)
-
-o1 =np.full((2,5),np.random.randint(1,10))
-print(o1)
-
-o2 = np.random.randint(0,10,size=(3,4))
-print(o2)
-print(o2.T)
+int=[4,7,8]
+print(o[4],o[7],o[8])
+print(o[int])
