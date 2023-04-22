1a0b15d30a6f920bfc4c6a79713eb5211b3d1b95
מיכאל <abbcnn220@gmail.com>
2023-04-22 23:24:09 +0300

    v 1.7

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 import time
 
-arr = np.array([[1,2,3,4,5],[6,7,8,9,10],[3,3,3,3,3]])
-print(arr.ndim)
-print(arr.shape)
+tempratures = np.array([29.3,42.1,18.8,38.0,12.5,12.6,12.7,49.9,38.6,31.3,9.2,22.2]).reshape(2,3,2)
+print(tempratures)
+print('----------')
+tempratures = np.swapaxes(tempratures,1,2)
+print(tempratures)
