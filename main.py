6b8407b7c3c2020577fd469f1d37fd3805e9dd2a
מיכאל <abbcnn220@gmail.com>
2023-04-22 22:38:44 +0300

    v 1.4 add numpy

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import numpy
+import numpy as np
+import sys
+import time
+
+
+size = 10000000
+l1 = range(size)
+l2 = range(size)
+a1=np.arange(size)
+a2=np.arange(size)
+start = time.time()
+# result = [(x+y) for (x,y) in zip(l1,l2)]
+result =a1+a2
+print(result)
+end = time.time()
+print('time:',end-start)
