51413733b3b6ab6eeb8a99de0eb7719356c27d42
מיכאל <abbcnn220@gmail.com>
2023-04-23 00:22:52 +0300

    v 1.11

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import time
 
-a1 = np.array([1,0,1,0])
-
-print(a1.astype('bool'))
-print((a1).shape)
+a1 = np.array([[1,2,3],[4,5,6]])
+a2 = np.array([[7,8,9],[10,11,12]])
+c =np.hstack((a1,a2))
+print(c)
