c68f1403c5cacfbeac1a41a2ed8b6bd274eeb90a
מיכאל <abbcnn220@gmail.com>
2023-04-22 22:07:07 +0300

    v 1.4 add numpy

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-langs = ['Java','Python','JavaScript']
-amount = [14,3,6]
-
-dict = {}
-combination = zip(langs,amount)
-for (k,v) in combination:
-    dict[k]=v
-print(dict)
-
-print({k:v for (k,v) in zip(langs,amount)})
+import numpy
