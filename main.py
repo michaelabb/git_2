57402b60747853ff764d288d77354097f1702d17
מיכאל <abbcnn220@gmail.com>
2023-04-23 02:09:52 +0300

    v 1.12

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import sys
 import time
 
-a1 = np.array([[1,2,3],[4,5,6]])
-a2 = np.array([[7,8,9],[10,11,12]])
-c =np.hstack((a1,a2))
-print(c)
+a1 = np.array([1,2,3,4,5,6])
+a2 = np.array([1,2,3,9,3,6])
+
+print(np.product(a1>a2))
+print(np.where(a1==a2))
+print(np.where(a1>a2))
