2b32836650c7ec28cc5e065ae98f8d58474f25d1
מיכאל <abbcnn220@gmail.com>
2023-04-23 00:09:21 +0300

    v 1.8

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import time
 
-a1 = np.array([[1,2,3],[4,5,6]])
-a2 = np.array([[7,8,9],[10,11,12]])
-print((a1+a2)*3.14)
-print((a1+a2).shape)
+a1 = np.array(range(27))
+
+print(a1.reshape((3,3,3)))
+print((a1).shape)
