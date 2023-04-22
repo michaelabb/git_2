8b157828f1efcd6a71078886c936fe16170ef555
מיכאל <abbcnn220@gmail.com>
2023-04-22 22:57:46 +0300

    v 1.5

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import numpy as np
 import sys
 import time
 
-
-size = 10000000
-l1 = range(size)
-l2 = range(size)
-a1=np.arange(size)
-a2=np.arange(size)
-start = time.time()
-# result = [(x+y) for (x,y) in zip(l1,l2)]
-result =a1+a2
-print(result)
-end = time.time()
-print('time:',end-start)
+arr = np.array([[1,2,3,4,5],[6,7,8,9,10],[3,3,3,3,3]])
+print(arr.ndim)
+print(arr.shape)
