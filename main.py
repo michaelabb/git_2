4823f6171bcb97b03fd5c440acef0e63252060b9
מיכאל <abbcnn220@gmail.com>
2023-04-23 03:02:51 +0300

    v 1.15

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
 import sys
 import time
-o =np.random.randint(100,size=10)
-print(o)
-int=[4,7,8]
-print(o[4],o[7],o[8])
-print(o[int])
+x = np.arange(1,101).reshape(10,10)
+print(x)
+print(x[2,3])
+print(x[2,[1,0,3]])
